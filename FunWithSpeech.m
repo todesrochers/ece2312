e9d10af1703eb11a1de2ab34cc150915424cf1be
amsalvalzo <112036095+amsalvalzo@users.noreply.github.com>
2023-01-31 17:29:49 -0500

    Add files via upload

--- FunWithSpeech.m
+++ FunWithSpeech.m
@@ -1,6 +1,13 @@
 function [] = FunWithSpeech(y, fs, Z)
 [y,fs] = audioread('Project1WAV.wav');
-length(y) %44100*5 = 220500
+length(y); %44100*5 = 220500
+y1 = y(:,1);
+zero_vec = zeros(length(y1),1);
+NewOutput = cat(2,y1,zero_vec);
 
+%saving to WAV file
+audiowrite("NewOutput.wav", NewOutput, fs);
+
+sound(NewOutput,fs)
 end
 
